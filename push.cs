e4cb5ccdf87377f48c30c53069f55ceb0bf41ba8
Shahzaib012 <shahzaibnawaz902@gmail.com>
2021-11-04 23:37:40 +0500

    first commit

---
+++ push.cs
@@ -0,0 +1,7 @@
+echo "# first_repo" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Shahzaib012/first_repo.git
+git push -u origin main
